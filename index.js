87c530552ee15439876c33727a23db792cd4635e
jcballaresdiaz@gmail.com <jcballaresdiaz@gmail.com>
2025-09-03 11:12:30 -0500

    Primer commit Node Js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hola mundito desde mi primera API en Node.js")
+})
+
+app.listen(port,()=>{
+    console.log(`API escuchando en http://localhost:${port}`);
+});
+
